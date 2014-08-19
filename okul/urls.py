803128245445e50acb0a4d51c1f1aa4f37b8833f
sly01 <aerkoc01@gmail.com>
2014-08-19 16:17:05 +0300

    urls updated

--- okul/urls.py
+++ okul/urls.py
@@ -1,24 +1,24 @@
 from django.conf.urls import patterns, include, url
-import yonetim.views
+from yonetim import views
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
     # Examples:
     # url(r'^$', 'okul.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^ogretim-elemanlari-listesi/', yonetim.views.ogretim_elemanlari_listesi),
-    url(r'^get-deneme/', yonetim.views.get_deneme),
-    url(r'^ogretim-elemani-ekleme', yonetim.views.ogretim_elemani_ekleme),
-    url(r'^coklu-ogretim-elemani-ekleme/', yonetim.views.coklu_ogretim_elemani_ekleme),
-    url(r'^ders-listesi/', yonetim.views.ders_listesi),
-    url(r'^ders-ekleme/', yonetim.views.ders_ekleme),
-    url(r'^ogrenci-listesi', yonetim.views.ogrenci_listesi),
-    url(r'^ogrenci-ekleme', yonetim.views.ogrenci_ekleme),
-    url(r'^yonetim', yonetim.views.yonetim),
-    url(r'^cerez-deneme', yonetim.views.cerez_deneme),
+    url(r'^ogretim-elemanlari-listesi/',views.ogretim_elemanlari_listesi),
+    url(r'^get-deneme/', views.get_deneme),
+    url(r'^ogretim-elemani-ekleme', views.ogretim_elemani_ekleme),
+    url(r'^coklu-ogretim-elemani-ekleme/', views.coklu_ogretim_elemani_ekleme),
+    url(r'^ders-listesi/', views.ders_listesi),
+    url(r'^ders-ekleme/', views.ders_ekleme),
+    url(r'^ogrenci-listesi', views.ogrenci_listesi),
+    url(r'^ogrenci-ekleme', views.ogrenci_ekleme),
+    url(r'^yonetim', views.yonetim),
+    url(r'^cerez-deneme', views.cerez_deneme),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/accounts/login/'}),
 )
